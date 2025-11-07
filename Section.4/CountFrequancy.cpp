ddf3c586447cc403cc87b1795cb2f6fe7a306015
imakasx <akashvishwakarma0618l@gmail.com>
2025-11-07 23:14:19 +0530

    Cpp-Practice

---
+++ Section.4/CountFrequancy.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter size: ";
+    cin>>n;
+    int arr[n], freq[n];
+    cout<<"Enter the array: ";
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+        freq[i]=-1;
+    }
+    for(int i=0; i<n; i++){
+        int count =1;
+        for(int j=i+1; j<n; j++){
+            if(arr[i]==arr[j]){
+                count++;
+                freq[j]=0;
+            }
+        }
+        if(freq[i]!=0){
+            freq[i]=count;
+        }
+    }
+    for(int i=0; i<n; i++){
+        if(freq[i]!=0){
+            cout<<arr[i]<<" occure "<<freq[i]<<" times "<<endl;
+        }
+    }
+}
