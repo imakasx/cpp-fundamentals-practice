e85ca59851cd16a394ebea663a331155e88c82a9
imakasx <akashvishwakarma0618@gmail.com>
2026-05-27 15:30:19 +0530

    Cpp-Practice

---
+++ Section.4/anagramsStr.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+bool anagrams(string str1 , string str2 ){
+    if(str1.length() != str2.length())
+    return false;
+
+    sort(str1.begin(), str1.end()); 
+    sort(str2.begin(), str2.end()); 
+    return (str1 == str2);
+    // cout<< (str1 == str2);
+}
+int main(){ 
+    string str1, str2; 
+    cout << "Enter first string: "; 
+    cin >> str1; 
+    cout << "Enter second string: "; 
+    cin >> str2; 
+
+    if (anagrams(str1, str2)) { 
+        cout << "Yes" << endl; 
+    } else { 
+        cout << "No" << endl; 
+    } 
+    return 0;
+}
